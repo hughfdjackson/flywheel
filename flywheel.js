3ba25570f6ee325c928bcd885da5c9c3b35e6e2d
Hugh Jackson <hughfdjackson@googlemail.com>
2011-09-26 18:05:16 +0100

    first prototype done

--- flywheel.js
+++ flywheel.js
@@ -1,8 +1,28 @@
 !function (context) {
     var flywheel = function(callback){
-        // paul irish shim 
-        var req_anim_frame =         
         
+//--------- BOILERPLATE ----------------//
+        
+        // paul irish shim 
+        var req_anim_frame = (function(){
+          return  window.requestAnimationFrame   || 
+              window.webkitRequestAnimationFrame || 
+              window.mozRequestAnimationFrame    || 
+              window.oRequestAnimationFrame      || 
+              window.msRequestAnimationFrame     ||
+              function(callback, element){
+                window.setTimeout(callback, 60)
+              }
+        })()        
+        
+        
+//------------- LOGIC ----------------------//
+
+        !function loop(){
+            callback()
+            req_anim_frame(loop)
+        }()
+
     }
     context['flywheel'] = flywheel
 }(this)
