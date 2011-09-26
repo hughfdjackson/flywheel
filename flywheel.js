51d84ed8a39e83dec400062ac7efad9d23b730b1
Hugh Jackson <hughfdjackson@googlemail.com>
2011-09-26 20:05:57 +0100

    clean up and introduced a controller object to allow you to stop and start the running of a particular loop at will

--- flywheel.js
+++ flywheel.js
@@ -1,6 +1,6 @@
 !function (context) {
+    
     var flywheel = function(callback){
-        
         // paul irish shim 
         var req_anim_frame = (function(){
           return  window.requestAnimationFrame   || 
@@ -11,25 +11,39 @@
               function(callback, element){
                 window.setTimeout(callback, 60)
               }
-        })()        
-        , last_frame_time = new Date().getTime()
-        
-        
-        function loop(timestamp){
-            var time_delta = timestamp - last_frame_time
-            last_frame_time = timestamp
+        })()
+
+        // loop logic
+        ,prev_timestamp = new Date().getTime()
+        ,run_loop = true
+        ,controller = {
+            stop: function(){
+                run_loop = false            
+            },
+            start: function(){
+                run_loop = true
+                req_anim_frame(loop)
+            }
+        }
+        ,loop(timestamp){
+            var time_delta = timestamp - prev_timestamp
+            prev_timestamp = timestamp
             
             // cap framerate 
-            if ( time_delta > 33 ) time_delta = 33
+            if(time_delta > 33) time_delta = 33
             
-            callback(time_delta, this)
-            req_anim_frame(loop)
+            callback(time_delta, controller)
+            
+            if(run_loop) req_anim_frame(loop)
 
         }
+
+        // initialise 
         req_anim_frame(loop)
+        
+        // return the controller
+        return controller
     }
-    
-    
     
     context['flywheel'] = flywheel
 }(this)
