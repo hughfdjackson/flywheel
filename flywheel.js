3cba8cb05b1b62d3722f4e707855a5b08540b8b8
Hugh Jackson <hughfdjackson@googlemail.com>
2011-09-26 18:02:47 +0100

    second commit

---
+++ flywheel.js
@@ -0,0 +1,8 @@
+!function (context) {
+    var flywheel = function(callback){
+        // paul irish shim 
+        var req_anim_frame =         
+        
+    }
+    context['flywheel'] = flywheel
+}(this)
