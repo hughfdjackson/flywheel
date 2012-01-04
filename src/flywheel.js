bd7059f24999aa9924b6c5a79826dd14d46aa7c6
hughfdjackson <hughfdjackson@googlemail.com>
2012-01-04 21:15:49 +0000

    refactoring progress on halt while I apply a bugfix discussed w/ src-test/spec/flywheel_spec.jspachet

--- src/flywheel.js
+++ src/flywheel.js
@@ -2,7 +2,7 @@ void function(root){
     
     // shim for cross-browser requestAnimationFrame, 
     // with setTimeout for a backup for older browser
-    var request_animation = function () {
+    var request_animation_frame = function () {
         return  window.requestAnimationFrame       
             ||  window.webkitRequestAnimationFrame 
             ||  window.mozRequestAnimationFrame
@@ -16,49 +16,63 @@ void function(root){
     }()
     
 
-    // controller 
-    // the object responsible 
+    //// controller : Object
+    // 
+    // gets returned from flywheel to let the
+    // user manipulate the looping
     var controller = {
         
         // --- Attributes --- // 
         callback:        undefined,
-        
+        element:         undefined,
+
         _last_timestamp: undefined,
         _running:        false,
+        
 
-    
         // --- Methods --- //
         start: function(){
             this._running = true
-            this.callback() 
+            this._next_frame() 
+            return this
         },
 
         stop: function(){
-            this._running = false 
+            this._running = false
+            return this
         },
         
         toggle: function(){
             this._running ? this.stop()
-            : /*otherwise*/ this.start() 
+            : /*otherwise*/ this.start()
+            return this
         },
 
         step: function(){
-            this.callback()
+            this._next_frame()
+            return this
         },
-
+    
+        _next_frame: function(){
         
+            request_animation_frame(function(){
+                callback
+            }, this.element)
+        }
     }
 
 
-    //// (callback : Function) -> controller : Object
+    //// (callback : Function[, element : HTMLElement ]) -> controller : Object
     //
     //  Public API for flywheel.  Just a wrapper around
     //  an instance of the controller object, really.
-    function flywheel(callback){
+    function flywheel(callback, element){
         
         var ret_controller = Object.create(controller)
         
         ret_controller.callback = callback
+        ret_controller.element = element
+
         ret_controller.constructor()
         
         return ret_controller 
